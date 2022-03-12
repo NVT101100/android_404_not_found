cfba4720aa113b1f71367a25b8bbf64bb9208397
NVT101100 <nguyenvanthoi.1011.bd@gmail.com>
2022-03-12 10:47:26 +0700

    signup

---
+++ tuan1/app/src/main/java/com/nvt/tuan1/Singup.kt
@@ -0,0 +1,11 @@
+package com.nvt.tuan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Singup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.singup)
+    }
+}
