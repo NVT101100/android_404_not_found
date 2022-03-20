a96edec0f234364bf92eb8442c9a358a20478227
NVT101100 <nguyenvanthoi.1011.bd@gmail.com>
2022-03-20 20:46:22 +0700

    create project w3

---
+++ tuan3/app/src/main/java/com/nvt/tuan3/Signup.kt
@@ -0,0 +1,11 @@
+package com.nvt.tuan3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup)
+    }
+}
