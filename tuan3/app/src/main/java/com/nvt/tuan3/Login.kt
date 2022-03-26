446d79c43bb5345fbd0dad0829da6558bfe1e184
NVT101100 <nguyenvanthoi.1011.bd@gmail.com>
2022-03-26 10:27:14 +0700

    complete challenge w3

--- tuan3/app/src/main/java/com/nvt/tuan3/Login.kt
+++ tuan3/app/src/main/java/com/nvt/tuan3/Login.kt
@@ -6,34 +6,53 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import com.nvt.tuan3.databinding.LoginBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding : LoginBinding
+    private var myModel : MyModel = MyModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
-
-        val acProfile:Intent = Intent(this,Profile::class.java)
-        val passwordText:EditText = findViewById(R.id.edittextpassword)
-        val userText:EditText = findViewById(R.id.edittextuser)
-        val btnLogin:Button = findViewById(R.id.btnLogin)
+        binding = DataBindingUtil.setContentView(this,R.layout.login)
+        val btnLogin = binding.btnLogin
 
         btnLogin.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                val password:String = passwordText.text.toString();
-                val user:String = userText.text.toString();
-                if(user.equals("ronaldo@gmail.com", ignoreCase = true)) {
-                    if(password.equals("123456")) {
-                        acProfile.putExtra("email",user);
-                        startActivity(acProfile);
-                    }
-                    else showMessage("Invalid password")
-                }
-                else showMessage("Invalid username")
+                onSubmit()
             }
         })
+        myModel.errEmail.observe(this, Observer { errEmail ->
+            updateView(binding.errEmailLG,myModel.errEmail.value)
+        })
+        myModel.errPassword.observe(this, Observer { errEmail ->
+            updateView(binding.errPassLG,myModel.errPassword.value)
+        })
+    }
+
+    private fun onSubmit() {
+        var email  = binding.edittextuser.text.toString()?.trim()
+        var password = binding.edittextpassword.text.toString()?.trim()
+        if(myModel.login(email,password)){
+            val acProfile:Intent = Intent(this,Profile::class.java)
+            acProfile.putExtra("user",User(DataStore.name,DataStore.email,DataStore.phone))
+            startActivity(acProfile)
+            finish()
+        }
+        else Toast.makeText(this,"Failed",Toast.LENGTH_SHORT)
     }
     private fun showMessage(message :String){
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+    private fun updateView(view:TextView,message:String?) {
+        if(message != "") {
+            view.text = message
+            view.visibility = View.VISIBLE
+        }
+        else view.visibility = View.GONE
     }
 }
